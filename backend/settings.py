277e786f9634aebd7a11aaf7d3fa2aaee503956d
MuhdHishamP <hishamp2131@gmail.com>
2024-05-25 15:36:42 +0530

    chore: Update .gitignore and requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -26,13 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@9$c4dq%wct3bzwj4#nv3x9)@ah+8m&*&e@0tm*7-#)ipq@4"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
-
-ALLOWED_HOSTS = ["*"]
-
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
